3e0abc5f57f1279267d6a04a627c12665b06366b
Alexandre Garbe <agarbe@sqli.com>
2017-10-14 22:14:01 +0200

    Replaced old images with new ones (also fixes #12 since they are smaller)

--- application/result_message.rb
+++ application/result_message.rb
@@ -6,16 +6,16 @@ class ResultMessage
 
   @@corresponding_hero_by_rank = {
 
-    1 => {name:"SUPER DUPONT",image_file:File.new("images/superdupont.jpeg","r")},
-    2 => {name:"CAPITAIN AMERICA",image_file:File.new("images/captainamerica.jpg","r")},
-    3 => {name:"FLASH",image_file:File.new("images/flash.jpg","r")},
-    4 => {name:"BATMAN",image_file:File.new("images/batman.jpg","r")},
-    5 => {name:"SPIDERMAN",image_file:File.new("images/spiderman.jpg","r")},
-    6 => {name:"WOLVERINE",image_file:File.new("images/wolverine.jpg","r")},
+    1 => {name:"SUPER DUPONT",image_file:File.new("images/superdupont.png","r")},
+    2 => {name:"CAPITAIN AMERICA",image_file:File.new("images/captainamerica.png","r")},
+    3 => {name:"FLASH",image_file:File.new("images/flash.png","r")},
+    4 => {name:"BATMAN",image_file:File.new("images/batman.png","r")},
+    5 => {name:"SPIDERMAN",image_file:File.new("images/spiderman.png","r")},
+    6 => {name:"WOLVERINE",image_file:File.new("images/wolverine.png","r")},
     7 => {name:"IRON MAN",image_file:File.new("images/ironman.png","r")},
-    8 => {name:"THOR",image_file:File.new("images/thor.jpg","r")},
-    9 => {name:"SUPERMAN",image_file:File.new("images/superman.jpg","r")},
-    10 => {name:"HULK",image_file:File.new("images/hulk.jpg","r")}
+    8 => {name:"THOR",image_file:File.new("images/thor.png","r")},
+    9 => {name:"SUPERMAN",image_file:File.new("images/superman.png","r")},
+    10 => {name:"HULK",image_file:File.new("images/hulk.png","r")}
 
   }
 
