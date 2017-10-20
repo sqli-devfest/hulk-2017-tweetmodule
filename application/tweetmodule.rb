562b2c5eb63d33f485020a62a10b7116a8a67a72
Alexandre Garbe <agarbe@sqli.com>
2017-10-20 09:08:44 +0200

    Only tweet rank 1 and 10

--- application/tweetmodule.rb
+++ application/tweetmodule.rb
@@ -28,7 +28,9 @@ def tweet(result_message)
   tweet_string = result_message.message
   tweet_image = result_message.corresponding_image
   puts tweet_string
-  @client.update_with_media(tweet_string,tweet_image)
+  if result_message.rank == 1 or result_message.rank == 10 then
+    @client.update_with_media(tweet_string,tweet_image)
+  end
 end
 
 #connect to the mqtt 'results topic' parse the result json messages and tweet them
